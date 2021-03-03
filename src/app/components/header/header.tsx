acec0877483cd7fdf7efb6c82426d9691f518cb9
Liza Krasnoshchok <nlight1122@gmail.com>
2021-03-03 13:24:03 +0200

    add header top

--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -4,9 +4,25 @@ import { Navigation } from './navigation/navigation';
 
 export const Header = () => {
   return (
-    <p>
-      header
-      <Navigation />
-    </p>
+    <div className="header">
+      <div className="header__top">
+        <div className="container">
+          <div className="header__top-line">
+            <a className="header__link" href="https://www.ukrposhta.ua/ua">
+              <img
+                className="header__logo"
+                alt="logo"
+                src="./images/logo.svg"
+              />
+            </a>
+            <div className="header__call-center">
+              <a className="header__tel" href="tel:0 800 300 545">0 800 300 545</a>
+              <p className="header__text">контакт-центр</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="header__nav"></div>
+    </div>
   )
 }
