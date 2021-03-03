b3c01b03905319ac70798c038af9060e8223e3b3
Liza Krasnoshchok <nlight1122@gmail.com>
2021-03-03 11:07:35 +0200

    initial

--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './header.sass';
+import { Navigation } from './navigation/navigation';
 
 export const Header = () => {
   return (
     <p>
       header
+      <Navigation />
     </p>
   )
 }
---
+++ src/app/components/header/navigation/navigation.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './navigation.sass';
+
+export const Navigation = () => {
+  return (
+    <p>
+      nav
+    </p>
+  )
+}
